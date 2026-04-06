d497994a390bd985a60cebdf027681408da332bd
Petko Kolev <petkokolev@Petkos-MacBook-Pro.local>
2026-04-06 17:56:04 +0100

    fix(terraform): enable remote state with S3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "petko-terraform-state"
+    key    = "cloud-resume/terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
 provider "aws" {
   region = "eu-west-2"
 }
